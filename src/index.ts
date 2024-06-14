b5c7b3418280f73501595f974066987581c0e442
Ammar Lakis <ammar.lakis@gmail.com>
2024-06-14 18:50:16 +0200

    Implement local statefile output fetching

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getInput, setOutput, info } from '@actions/core';
+
+interface LocalBackendConfiguration {
+    backendType : 'local';
+    path : string;
+}
+
+type BackendConfiguration = LocalBackendConfiguration;
+
+function getOutputFromLocal(stateFilePath: string): Record<string, any>{
+    const result: Record<string, any> = {};
+    const contents = fs.readFileSync(path.resolve(stateFilePath));
+    const outputs = JSON.parse(contents.toString()).outputs;
+    for (var key in outputs) {
+        if (outputs.hasOwnProperty(key)) {
+            result[key] = outputs[key].value;
+        }
+    }
+    return result;
+}
+
+
+
+function main() {
+    const backendConfiguration: BackendConfiguration = JSON.parse(getInput('backend-configuration'));
+    let stateFilePath: string;
+
+    switch (backendConfiguration.backendType) {
+        case 'local':
+            stateFilePath = (backendConfiguration as LocalBackendConfiguration).path;
+            break;
+        default:
+            throw new Error('Unsupported backend type: ' + backendConfiguration.backendType);
+    }
+    const result = getOutputFromLocal(stateFilePath);
+    setOutput('value', result);
+}
+
+main();
